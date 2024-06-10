dbd0795d2903cf918477e7d768929bd7b3c3fafe
Logan Weigel <snr.wiggels@gmail.com>
2024-06-09 21:16:14 -0500

    modify src path for flagged images on admin page

--- src/components/Admin/FeedbackItem.jsx
+++ src/components/Admin/FeedbackItem.jsx
@@ -33,13 +33,13 @@ export default function FeedbackItem({ feedbackItem, fetchFeedback }) {
       <td onClick={flagFeedback}>
         {feedbackItem.flagged === true ? (
           <img
-            src='/images/checked.png'
+            src='../../../../images/checked.png'
             alt='Flagged'
             width={24}
           />
         ) : (
           <img
-            src='/images/delete.png'
+            src='../../../../images/delete.png'
             alt='Not Flagged'
             width={24}
           />
